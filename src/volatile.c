06ecf10ffb882dd90947bf16b65d1af1b2f1a9c4
tnn4 <tnn130130@gmail.com>
2023-07-19 06:19:24 -0500

    cleanup

--- src/volatile.c
+++
@@ -1,5 +0,0 @@
-
-// What is the purpose of volatile?
-// We use volatile when we want the program to not optimize and cache a value
-// Why do we need this?
-// c: https://stackoverflow.com/questions/246127/why-is-volatile-needed-in-c
