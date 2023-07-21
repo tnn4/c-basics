ec50eba988fcfef1a7aed9e4f535db9ee5de3da6
tnn4 <tnn130130@gmail.com>
2023-07-21 11:54:05 -0500

    add iterate list

--- src/linked_list.c
+++ src/linked_list.c
@@ -3,12 +3,15 @@
 #include <string.h> // strcpy
 
 #define DEBUG 1
+#define NODE_SIZE 31
 #define BUFSIZE 128
 
 void println(char *);
+struct Node* add_to_list(struct Node *, char *);
+void iterate_list(struct Node *);
 
 struct Node {
-    char value[16];
+    char value[NODE_SIZE + 1];
     struct Node *next;
 };
 
@@ -37,10 +40,46 @@ int main(void) {
 
     printf("\n%s", new_node->value);
 
+    first = add_to_list(first, "another node");
+    first = add_to_list(first, "another node 2");
+
+    iterate_list(first);
+
     println("\n[OK]\n");
     return 0;
 }
 
 void println(char *astring){
     printf("%s\n", astring);
+}
+
+// list - pointer to first node in old list
+// value - string to store in new node
+struct Node* add_to_list(struct Node *list, char *value){
+    struct Node *new_node;
+    new_node = malloc(sizeof(struct Node));
+
+    if (new_node == NULL){
+        printf("Error: malloc failed in add_to_list\n");
+        exit(EXIT_FAILURE);
+    }
+
+    //new_node->value = value;
+    strcpy(new_node->value, value);
+    new_node->next = list;
+
+    return new_node;
+}
+
+// print out values of the linked list
+void iterate_list(struct Node *list){
+    int i = 0;
+    while( list != NULL){
+        printf("[%d]:%s\n", i, list->value);
+        list = list->next;
+        i++;
+        if (i == 100){
+            break;
+        }
+    }
 }
